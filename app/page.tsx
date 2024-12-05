f167276b5d24f4fd5aa8003dc297e69a515ed272
Adam Bardsley <adam.bardsley@gmail.com>
2024-12-05 09:17:30 +0000

    Homepage specifically linked

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ import { components } from "@/slices";
 
 export default async function Page() {
   const client = createClient();
-  const page = await client.getSingle("homepage");
+  const page = await client.getByUID("page","homepage");
 
   return <SliceZone slices={page.data.slices} components={components} />;
 }
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
-  const page = await client.getByID("homepage");
+  const page = await client.getByUID("page","homepage");
 
   return {
     title: page.data.meta_title,
